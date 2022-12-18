211ec18e72bb66c31d73b0808dc2dda4edbaa10a
Jurasia <jurasiabritton@gmail.com>
2022-12-18 14:34:09 -0500

    commit tree 3cb9592a7a7704cccff8ce66b6443ebedcb35672

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+
+
+ const  writeCards = (["Guadalupe", "Ollie", "Aki"], "surprise!"); () =>
+   ["Thank you, Guadalupe, for the wonderful surprise gift!",
+  "Thank you, Ollie, for the wonderful surprise gift!",
+  "Thank you, Aki, for the wonderful surprise gift!",];
